956e15af3345583b24bc21193fbc0cf6b3c91db1
Terria028 <86240437+Terria-K@users.noreply.github.com>
2024-01-27 00:40:47 +0800

    Fixed tilemap

--- Core/Component/Tilemap.cs
+++ Core/Component/Tilemap.cs
@@ -17,6 +17,7 @@ public class Tilemap : Component
     private Texture frameBuffer;
     private bool dirty = true;
     private TilemapMode mode;
+    private Matrix4x4 Matrix;
     public int GridSize;
 
 
@@ -25,11 +26,20 @@ public class Tilemap : Component
     {
         int rows = tiles.Rows;
         int columns = tiles.Columns;
+
+        var model = Matrix4x4.CreateScale(1) *
+            Matrix4x4.CreateRotationZ(0) *
+            Matrix4x4.CreateTranslation(0, 0, 0);
+        var view = Matrix4x4.CreateTranslation(0, 0, 0);
+        var projection = Matrix4x4.CreateOrthographicOffCenter(0, rows * gridSize, 0, columns * gridSize, -1, 1);
+
+        Matrix = model * view * projection;
+
         this.tiles = tiles;
         this.tilemapTexture = texture;
         GridSize = gridSize;
         frameBuffer = Texture.CreateTexture2D(GameContext.GraphicsDevice,
-            1024, 640,
+            (uint)(rows * gridSize), (uint)(columns * gridSize),
             TextureFormat.R8G8B8A8, TextureUsageFlags.Sampler | TextureUsageFlags.ColorTarget);
         this.mode = mode;
     }
@@ -63,7 +73,7 @@ public class Tilemap : Component
                 AddToBatch(spriteBatch, ref buffer);
                 buffer.BeginRenderPass(new ColorAttachmentInfo(frameBuffer, Color.Transparent));
                 buffer.BindGraphicsPipeline(GameContext.DefaultPipeline);
-                spriteBatch.Draw(buffer);
+                spriteBatch.Draw(buffer, Matrix);
                 buffer.EndRenderPass();
                 dirty = false;
             }
