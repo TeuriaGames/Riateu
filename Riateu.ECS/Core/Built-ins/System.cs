d08d4cbf6424261782c0aac5499ad2c8f53e7788
Terria-K <aelbrian028@gmail.com>
2024-07-11 01:07:04 +0800

    Added helper function inside of System

--- Riateu.ECS/Core/Built-ins/System.cs
+++ Riateu.ECS/Core/Built-ins/System.cs
@@ -4,8 +4,28 @@ public abstract class System
 {
     private World world;
     public World World => world;
+    public SearchBuilder Search => World.Search;
+
     public System(World world) 
     {
         this.world = world;
+    }
+
+    public void Add<T>(in EntityID entity, in T component) 
+    where T : unmanaged
+    {
+        World.AddComponent(entity, component);
+    }
+
+    public ref T Get<T>(in EntityID entity) 
+    where T : unmanaged
+    {
+        return ref World.GetComponent<T>(entity);
+    }
+
+    public bool Remove<T>(in EntityID entity) 
+    where T : unmanaged
+    {
+        return World.RemoveComponent<T>(entity);
     }
 }
