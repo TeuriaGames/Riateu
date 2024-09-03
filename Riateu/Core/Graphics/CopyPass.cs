bd8e06c21d4c32aa1925ee4b2679b85879f52b93
Terria-K <teuria@proton.me>
2024-09-04 00:56:19 +0800

    Download

--- Riateu/Core/Graphics/CopyPass.cs
+++ Riateu/Core/Graphics/CopyPass.cs
@@ -31,6 +31,26 @@ public class CopyPass : IPassPool
 		Refresh.Refresh_UploadToTexture(Handle, source.ToSDLGpu(), destination.ToSDLGpu(), cycle ? 1 : 0);
 	}
 
+    public void CopyTextureToTexture(in TextureLocation source, in TextureLocation destination, uint w, uint h, uint d, bool cycle) 
+    {
+        Refresh.Refresh_CopyTextureToTexture(Handle, source.ToSDLGpu(), destination.ToSDLGpu(), w, h, d, cycle ? 1 : 0);
+    }
+
+    public void CopyBufferToBuffer(in BufferLocation source, in BufferLocation destination, uint size, bool cycle) 
+    {
+        Refresh.Refresh_CopyBufferToBuffer(Handle, source.ToSDLGpu(), destination.ToSDLGpu(), size, cycle ? 1 : 0);
+    }
+
+    public void DownloadFromBuffer(in BufferRegion source, in TransferBufferLocation destination) 
+    {
+        Refresh.Refresh_DownloadFromBuffer(Handle, source.ToSDLGpu(), destination.ToSDLGpu());
+    }
+
+    public void DownloadFromTexture(in TextureRegion source, in TextureTransferInfo destination) 
+    {
+        Refresh.Refresh_DownloadFromTexture(Handle, source.ToSDLGpu(), destination.ToSDLGpu());
+    }
+
     public void Obtain(nint handle)
     {
         Handle = handle;
