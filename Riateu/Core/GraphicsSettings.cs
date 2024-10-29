6f75e2a7be36656724443697b46c9b488dfd86de
Terria-K <teuria@proton.me>
2024-10-29 22:04:57 +0800

    Fixed typo

--- Riateu/Core/GraphicsSettings.cs
+++ Riateu/Core/GraphicsSettings.cs
@@ -7,7 +7,7 @@ public record struct GraphicsSettings(SwapchainComposition SwapchainComposition,
     public static readonly GraphicsSettings Default = new GraphicsSettings(SwapchainComposition.SDR, PresentMode.Immediate, false, false);
     public static readonly GraphicsSettings Debug = new GraphicsSettings(SwapchainComposition.SDR, PresentMode.Immediate, true, false);
     public static readonly GraphicsSettings Vsync = new GraphicsSettings(SwapchainComposition.SDR, PresentMode.VSync, false, false);
-    public static readonly GraphicsSettings DebugVSync = new GraphicsSettings(SwapchainComposition.SDR, PresentMode.VSync, true, false);
+    public static readonly GraphicsSettings DebugVsync = new GraphicsSettings(SwapchainComposition.SDR, PresentMode.VSync, true, false);
     public static readonly GraphicsSettings Fast = new GraphicsSettings(SwapchainComposition.SDR, PresentMode.Mailbox, false, false);
     public static readonly GraphicsSettings DebugFast = new GraphicsSettings(SwapchainComposition.SDR, PresentMode.Mailbox, true, false);
 }
